7d7baeb443d1b27ed2a2983ac4fb106dbce799ef
nastya-randall <nastya-randall@users.noreply.github.com>
2018-06-07 14:43:10 +0200

    Problem with randomizing wizards

---
+++ js/setup.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var firstNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var lastNames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var coats = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyes = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var wizards = [];
+var wizard = {};
+
+var randomizeWizard = function () {
+  for (var i = 0; i < 4; i++) {
+    wizards.push(wizard);
+    var randomName = firstNames[Math.floor(Math.random() * firstNames.length)] + ' ' + lastNames[Math.floor(Math.random() * lastNames.length)];
+    wizards[i].name = randomName;
+    wizards[i].coatColor = coats[Math.floor(Math.random() * coats.length)];
+    wizards[i].eyesColor = eyes[Math.floor(Math.random() * eyes.length)];
+  }
+};
+
+randomizeWizard();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+
+document.querySelector('.setup-similar').classList.remove('hidden');
