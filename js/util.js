2128b5a4134de2c96f925ccd48297ae70cff1cc0
Unknown <anastassiya.sviridova@gmail.com>
2018-06-22 09:16:52 +0200

    fixed

--- js/util.js
+++ js/util.js
@@ -4,12 +4,6 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
-  window.isEscEvent = function (evt, action) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      action();
-    }
-  };
-
   window.util = {
     isEscEvent: function (evt, action) {
       if (evt.keyCode === ESC_KEYCODE) {
